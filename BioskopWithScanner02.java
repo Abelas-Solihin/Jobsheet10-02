afb421f1c04cb19b08c5d51160110cbc37295fff
Abelas-Solihin <abelassolihin@gmail.com>
2024-11-07 11:31:13 +0700

    modifikasi

--- BioskopWithScanner02.java
+++ BioskopWithScanner02.java
@@ -1,26 +1,51 @@
 import java.util.Scanner;
+
 public class BioskopWithScanner02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String nama, next;
         int baris, kolom;
+        int pilihan;
 
-        String [][] penonton = new String [4][2];
+        String[][] penonton = new String[4][2];
+
         while (true) {
-            System.out.println("masukkan nama : ");
-            nama = sc.nextLine();
-            System.out.println("Masukkan baris: ");
-            baris = sc.nextInt();
-            System.out.println("Masukkan kolom : ");
-            kolom = sc.nextInt();
-            sc.nextLine();
-            penonton[baris-1][kolom-1] = nama;
+            System.out.println("Menu:");
+            System.out.println("1. Input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit");
+            System.out.print("Pilih menu (1/2/3): ");
+            pilihan = sc.nextInt();
+            sc.nextLine(); 
 
-            System.out.println("Input penonton lainnya (y/n: )");
-            next = sc.nextLine();
-
-            if (next.equalsIgnoreCase("n")){
+            if (pilihan == 1) {
+                System.out.println("Masukkan nama : ");
+                nama = sc.nextLine();
+                System.out.println("Masukkan baris: ");
+                baris = sc.nextInt();
+                System.out.println("Masukkan kolom : ");
+                kolom = sc.nextInt();
+                sc.nextLine();
+                if (baris >= 1 && baris <= 4 && kolom >= 1 && kolom <= 2) {
+                    penonton[baris - 1][kolom - 1] = nama;
+                    System.out.println("Data penonton berhasil dimasukkan.");
+                } else {
+                    System.out.println("Baris atau kolom tidak valid.");
+                }
+            } else if (pilihan == 2) {
+                System.out.println("Daftar penonton:");
+                for (int i = 0; i < penonton.length; i++) {
+                    for (int j = 0; j < penonton[i].length; j++) {
+                        if (penonton[i][j] != null) {
+                            System.out.println("Baris " + (i + 1) + ", Kolom " + (j + 1) + ": " + penonton[i][j]);
+                        }
+                    }
+               }
+            } else if (pilihan == 3) {
+                System.out.println("Terima kasih! proses selesai.");
                 break;
+            } else {
+                System.out.println("Pilihan tidak valid. Silakan coba lagi.");
             }
         }
     }
--- Numbers02.java
+++ Numbers02.java
@@ -9,7 +9,8 @@ import java.util.Scanner;
             myNumbers[1] = new int[3];
             myNumbers[2] = new int[1];
 
-          
-            
+            for (int i = 0; i < myNumbers.length; i++){
+                System.out.println(Arrays.toString(myNumbers[i]));
+            }
         }
     }
